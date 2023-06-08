4e485d2aa0f7e882a3b085f8fb434a4833e490d5
devkapsakrisz <devkapsakrisz@gmnail.com>
2023-06-08 13:04:54 +0100

    6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './page/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
